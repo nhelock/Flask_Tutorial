645e9456a48175f0e62688540b5fa942df96f2a3
Yusuf <bsse1013@iit.du.ac.bd>
2022-02-14 11:10:48 +0600

    templates basic

--- main.py
+++ main.py
@@ -1,38 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+# render a file
 @app.route('/')
 def index():
-    return '<h1>Hello World!!</h1>'
+    return render_template('index.html')
 
 
-# adding more routes
-@app.route('/hello')
-def hello():  # defining index() again would have given an error
-    return 'Hey there!'
-
-
-# variable input in route
-# try localhost:5000/john, or localhost:5000/mary
-@app.route('/hello/<name>')  # name is the input var
-def greet(name):  # name is passed down as parameter
-    return 'Hey there, ' + name + '!'
-
-
-# specifying data type
-@app.route('/hello_str/<string:name>')  # don't put any spaces in url
-def greet_string(name):
-    return 'Hey there!' + name
-
-
-# int data type. Try -
-# localhost:5000/hello_num/1
-# localhost:5000/hello_num/one
-@app.route('/hello_num/<int:num>')
-def next_number(num):
-    return 'Next number is ' + str(num + 1)
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
