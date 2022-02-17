cd16596a3f373b52e83c8e4bd3c772aa6828e292
Yusuf <bsse1013@iit.du.ac.bd>
2022-02-18 00:10:03 +0600

    create

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from sqlalchemy import Column, Integer, String, Numeric, create_engine, text
 
 app = Flask(__name__)
 conn_str = "mysql://root:iit123@localhost/boatdb"
 engine = create_engine(conn_str, echo=True)
 conn = engine.connect()
+
 
 # create model
 class BoatsModel:
@@ -31,18 +31,37 @@ def user(name):
 
 
 # get all boats
+# this is done to handle requests for two routes -
 @app.route('/boats/')
 @app.route('/boats/<page>')
 def get_boats(page=1):
-    # boats = BoatsModel.query.paginate(page=int(page), per_page=10)  # returns all boats
-    page = int(page)
-    per_page = 10
-    boats = conn.execute(text(f"SELECT * FROM boats LIMIT {per_page} OFFSET {(page-1)*per_page}")).all()
+    page = int(page)  # request params always come as strings. So type conversion is necessary.
+    per_page = 10  # records to show per page
+    boats = conn.execute(text(f"SELECT * FROM boats LIMIT {per_page} OFFSET {(page - 1) * per_page}")).all()
     print(boats)
     return render_template('boats.html', boats=boats, page=page, per_page=per_page)
 
 
+@app.route('/create', methods=['GET'])
+def create_get_request():
+    return render_template('boats_create.html')
 
+
+@app.route('/create', methods=['POST'])
+def create_boat():
+    # you can access the values with request.from.name
+    # this name is the value of the name attribute in HTML form's input element
+    # ex: print(request.form['id'])
+    try:
+        conn.execute(
+            text("INSERT INTO boats values (:id, :name, :type, :owner_id, :rental_price)"),
+            request.form
+        )
+        return render_template('boats_create.html', error=None, success="Data inserted successfully!")
+    except Exception as e:
+        error = e.orig.args[1]
+        print(error)
+        return render_template('boats_create.html', error=error, success=None)
 
 
 
